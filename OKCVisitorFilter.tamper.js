93cb16b2407c299557577994e6e1b7a4f6ddd9bd
Marie Huynh <mariethuynh@gmail.com>
2014-03-19 07:11:03 -0700

    Update for recent changes in the UI

--- OKCVisitorFilter.tamper.js
+++ OKCVisitorFilter.tamper.js
@@ -1,17 +1,17 @@
 // ==UserScript==
 // @name        OkCupid "Visitor" Filter
 // @description Hides "visitors" of < 80% match to increase usefulness of the page
-// @version     0.1
+// @version     0.2
 // @namespace   http://mariehuynh.com/
 // @include     http://www.okcupid.com/visitors
 // @require     http://code.jquery.com/jquery-latest.js
 // ==/UserScript==
 
 $(".user_row_item").each(function( index ) {
-    match = $( this ).children(".match_percentages")
-                     .children(".match")
+    match = $( this ).children(".match_percentage")
+                     .children(".percentage")
                      .text();
                      
-    if(parseInt(match, 10) < 80)
+    if(parseInt(match,10) < 80)
         $( this ).hide();
 });
