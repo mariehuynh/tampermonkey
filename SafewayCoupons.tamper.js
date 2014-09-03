c505486b938edd9d2d27b7743450004054f46aba
Marie Huynh <mariethuynh@gmail.com>
2014-09-03 06:26:37 -0700

    Safeway Coupon clicker

---
+++ SafewayCoupons.tamper.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name        Safeway Coupons
+// @description Adds all Safeway Coupons to card so I don't have to click 400x
+// @version     0.1
+// @namespace   http://mariehuynh.com/
+// @match       http://www.safeway.com/ShopStores/*
+// @require     http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
+
+$('body').prepend('<input type="button" value="Click All Add" id="button1">')
+
+$('#button1').click(function(){
+    $(".lt-button-primary-add").trigger("click");
+    return false;
+});
