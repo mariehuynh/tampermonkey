136749b102f33afeb8d77a858f1cd71291ffdb10
Marie Huynh <mariethuynh@gmail.com>
2014-11-03 16:09:35 -0800

    Add comments and clean up

--- GSXSales.tamper.js
+++ GSXSales.tamper.js
@@ -11,7 +11,7 @@ $( document ).ready(function() {
     $('#body').prepend('<input type="button" value="Hide All Non-Sales" id="button1" style="top: 22px;z-index: 1000; position: absolute; left: 813px;">');
 
     $('#button1').click(function(){
-        items = $(".contentArea li").hide();
-      	$(".GNVEIADBJUF").closest("li").show();
+        items = $(".contentArea li").hide();     // Hides all the items
+      	$(".GNVEIADBJUF").closest("li").show();  // Shows the sale items; this class may well change in the future
     });
-});    
+});
