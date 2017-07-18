6128cfe65ef8593fa6e1c1d544cc4c1f240bbc4c
Marie Huynh <mariethuynh@gmail.com>
2017-07-18 11:53:08 -0700

    Create ReversoAds.tamper.js

---
+++ ReversoAds.tamper.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        Reverso ad block
+// @description Hides annoying ads
+// @version     0.2
+// @namespace   http://mariehuynh.com/
+// @include     *context.reverso.net/translation*
+// @require     http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
+
+$('#right-pan').hide();
+$('#bottom-mega-rca-box').hide();
