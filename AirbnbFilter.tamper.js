faae1adb1aeb823961473d44ee5137e460f0ca1d
Marie Huynh <mariethuynh@gmail.com>
2017-09-06 09:47:14 -0700

    cleanup

--- AirbnbFilter.tamper.js
+++ AirbnbFilter.tamper.js
@@ -8,32 +8,28 @@
 // ==/UserScript==
 
 $( document ).ready(function() {
-    $('body').prepend('<input type="button" value="Temp hide < 90% Locally" id="TH">');
-    //$('body').prepend('<input type="button" value="Hide < 90% on Server" id="SH">');
 
-    // Hide all listing cards
-    // Reload the page to reset
     $('.cardContainer_gig1e7').each(function( index ) {
-        url = $( this ).find("a.anchor_surdeb").attr('href');
+        url = $(this).find("a.anchor_surdeb").attr('href');
         var that = this;
 
         // Fetch listings and search
-        $.get(url).done(function( data ) {
-            console.log("data returned");
+        $.get(url).done(function(data) {
+            //console.log("data returned");
 
             // Smoking allowed?
             smoke = data.search('allows_smoking":t');
-            console.log("smoke: "+smoke);
+            //console.log("smoke: " + smoke);
 
             // Has pets?
             pets = data.search('_pets":t');
-            console.log("pets: "+pets);
+            //console.log("pets: " + pets);
 
             // pets and smoke are -1 if not found; probably big number if found
             allergens = pets + smoke;
 
             // Hide container
-            if(allergens > 0 ) {
+            if(allergens > 0) {
                 console.log('hide it');
                 $(that).hide();
             }
