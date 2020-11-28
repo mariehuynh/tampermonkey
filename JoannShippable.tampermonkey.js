eb212670d0d1b3bf84573d39806783f34ea442fe
Marie Huynh <mariethuynh@gmail.com>
2020-11-27 19:55:04 -0800

    Create JoannShippable.tampermonkey.js

---
+++ JoannShippable.tampermonkey.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        Joann shippable
+// @description Hide nonsales on GSX
+// @version     0.1
+// @namespace   http://mariehuynh.com/
+// @match       https://www.joann.com/*
+// @require     http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
+$( document ).ready(function() {
+
+    $('body').prepend('<input type="button" value="Hide All Non-Shipping" id="button1" style="top: 22px;z-index: 1000; position: absolute; left: 813px;">');
+
+    $('#button1').click(function(){
+      	$(".product-grid__item:contains('Not available for shipping')").hide();  // Shows the sale items; this class may well change in the future
+    });
+});
